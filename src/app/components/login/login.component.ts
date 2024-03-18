5aa00a8b4fac76b203b17ff1bcbe3a9d4c11d191
Zwogli <Mathiaskohler@web.de>
2024-03-18 16:58:09 +0100

    Add login endpoint frontend

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,14 +6,40 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  email: string = '';
+  username: string = '';
   password: string = '';
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  login() {
-    // Logic, communicate with backend
+  async loginWithUsernameAndPAssword() {
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+
+    const raw = JSON.stringify({
+      username: this.username,
+      password: this.password,
+    });
+
+    const requestOptions: RequestInit = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+    };
+
+    try {
+      // disable input and button
+      let response = await fetch(
+        'http://127.0.0.1:8000/login/',
+        requestOptions
+      );
+      let json = await response.json();
+      localStorage.setItem('token', json.token);
+      // TODO: Redirect
+    } catch (e) {
+      console.error(e); // Show error message
+    }
   }
 }
