3dc2b364d38bd85bfe6513f0113177c6b78163e3
Zwogli <Mathiaskohler@web.de>
2024-03-21 09:27:54 +0100

    Init auth service

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,5 @@
 import { Component } from "@angular/core";
-import { environment } from "../../../environments/environment";
-import { HttpClient } from "@angular/common/http";
-import { lastValueFrom } from "rxjs";
+import { AuthService } from "../../services/auth/auth.service";
 
 @Component({
 	selector: "app-login",
@@ -12,28 +10,18 @@ export class LoginComponent {
 	username: string = "";
 	password: string = "";
 
-	constructor(private http: HttpClient) {}
+	constructor(private as: AuthService) {}
 
 	ngOnInit(): void {}
 
 	async login() {
 		try {
 			// disable input and button
-			let response = await this.loginWithUsernameAndPassword(this.username, this.password);
+			let response = await this.as.loginWithUsernameAndPassword(this.username, this.password);
 			console.log("Show response: ", response);
-
 			// TODO: Redirect
 		} catch (e) {
 			console.error(e); // Show error message
 		}
-	}
-
-	loginWithUsernameAndPassword(username: string, password: string) {
-		const url = environment.baseUrl + "/login/";
-		const body = {
-			"username": username,
-			"password": password,
-		};
-		return lastValueFrom(this.http.post(url, body));
 	}
 }
---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../../environments/environment";
+import { lastValueFrom } from "rxjs";
+
+@Injectable({
+	providedIn: "root",
+})
+export class AuthService {
+	constructor(private http: HttpClient) {}
+
+	public loginWithUsernameAndPassword(username: string, password: string) {
+		const url = environment.baseUrl + "/login/";
+		const body = {
+			"username": username,
+			"password": password,
+		};
+		return lastValueFrom(this.http.post(url, body));
+	}
+}
