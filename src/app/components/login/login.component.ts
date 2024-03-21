270d5d4ef92cb3554959cae7d255082ee0a2002a
Zwogli <Mathiaskohler@web.de>
2024-03-21 09:09:21 +0100

    Fix prettier formatter

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,42 +1,39 @@
-import { Component } from '@angular/core';
-import { environment } from '../../../environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { lastValueFrom } from 'rxjs';
+import { Component } from "@angular/core";
+import { environment } from "../../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { lastValueFrom } from "rxjs";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
+	selector: "app-login",
+	templateUrl: "./login.component.html",
+	styleUrl: "./login.component.scss",
 })
 export class LoginComponent {
-  username: string = '';
-  password: string = '';
+	username: string = "";
+	password: string = "";
 
-  constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {}
+	ngOnInit(): void {}
 
-  async login() {
-    try {
-      // disable input and button
-      let response = await this.loginWithUsernameAndPassword(
-        this.username,
-        this.password
-      );
-      console.log('Show response: ', response);
+	async login() {
+		try {
+			// disable input and button
+			let response = await this.loginWithUsernameAndPassword(this.username, this.password);
+			console.log("Show response: ", response);
 
-      // TODO: Redirect
-    } catch (e) {
-      console.error(e); // Show error message
-    }
-  }
+			// TODO: Redirect
+		} catch (e) {
+			console.error(e); // Show error message
+		}
+	}
 
-  loginWithUsernameAndPassword(username: string, password: string) {
-    const url = environment.baseUrl + '/login/';
-    const body = {
-      username: username,
-      password: password,
-    };
-    return lastValueFrom(this.http.post(url, body));
-  }
+	loginWithUsernameAndPassword(username: string, password: string) {
+		const url = environment.baseUrl + "/login/";
+		const body = {
+			"username": username,
+			"password": password,
+		};
+		return lastValueFrom(this.http.post(url, body));
+	}
 }
