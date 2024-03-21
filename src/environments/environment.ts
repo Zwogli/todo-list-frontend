26c17ec973664d962e58b199e9cfa7933c6581ba
Zwogli <Mathiaskohler@web.de>
2024-03-21 09:22:24 +0100

    Fix backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  baseUrl: 'http://localhost:4200',
+	production: false,
+	baseUrl: "http://127.0.0.1:8000",
 };
 
 /*
