04144172f0815212af9b9ae7a77a1c048c271a14
Mathias Kohler <123494012+Zwogli@users.noreply.github.com>
2024-03-22 22:31:18 +0100

    Create router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./components/login/login.component";
+import { AllTodosComponent } from "./components/all-todos/all-todos.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: "", redirectTo: "login", pathMatch:'full' },
+	{ path: "login", component: LoginComponent },
+	{ path: "todos", component: AllTodosComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
