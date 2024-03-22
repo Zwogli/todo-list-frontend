77cb1284904c9bef41ba4fa317c86f8adb53bf92
Mathias Kohler <123494012+Zwogli@users.noreply.github.com>
2024-03-22 22:47:24 +0100

    Add render todos

--- src/app/components/all-todos/all-todos.component.ts
+++ src/app/components/all-todos/all-todos.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component } from "@angular/core";
+import { environment } from "../../../environments/environment";
+import { lastValueFrom } from "rxjs";
 
 @Component({
-  selector: 'app-all-todos',
-  templateUrl: './all-todos.component.html',
-  styleUrl: './all-todos.component.scss'
+	selector: "app-all-todos",
+	templateUrl: "./all-todos.component.html",
+	styleUrl: "./all-todos.component.scss",
 })
 export class AllTodosComponent {
+	todos: any = [];
 
+	constructor(private http: HttpClient) {}
+
+	async ngOnInit() {
+		this.todos = await this.loadTodos();
+		console.log("Show all Todos: ", this.todos);
+	}
+
+	loadTodos() {
+		const url = environment.baseUrl + "/todos/";
+		return lastValueFrom(this.http.get(url));
+	}
 }
