ac7a5844506dce2484c06ae7445d6a26d17a6200
Mathias Kohler <123494012+Zwogli@users.noreply.github.com>
2024-03-23 00:18:46 +0100

    Add authentication for todos

--- src/app/components/all-todos/all-todos.component.ts
+++ src/app/components/all-todos/all-todos.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Component } from "@angular/core";
 import { environment } from "../../../environments/environment";
 import { lastValueFrom } from "rxjs";
@@ -10,16 +10,30 @@ import { lastValueFrom } from "rxjs";
 })
 export class AllTodosComponent {
 	todos: any = [];
+	error = "";
 
 	constructor(private http: HttpClient) {}
 
 	async ngOnInit() {
-		this.todos = await this.loadTodos();
-		console.log("Show all Todos: ", this.todos);
+		try {
+			this.todos = await this.loadTodos();
+			console.log("Show all Todos: ", this.todos);
+		} catch (e) {
+			this.error = "Fehler beim laden!";
+		}
 	}
 
 	loadTodos() {
 		const url = environment.baseUrl + "/todos/";
-		return lastValueFrom(this.http.get(url));
+		let headers = new HttpHeaders();
+		headers = headers.set(
+			"Authorization",
+			"Token " + localStorage.getItem("token")
+		);
+		return lastValueFrom(
+			this.http.get(url, {
+				headers: headers,
+			})
+		);
 	}
 }
